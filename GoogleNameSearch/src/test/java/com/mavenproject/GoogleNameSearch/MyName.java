16d3c8f53843f2e4a16ddce060ab147ed8e3be00
Esha <esha.vij@appdirect.com>
2017-03-22 12:18:54 +0530

    Update MyName.java

--- GoogleNameSearch/src/test/java/com/mavenproject/GoogleNameSearch/MyName.java
+++ GoogleNameSearch/src/test/java/com/mavenproject/GoogleNameSearch/MyName.java
@@ -13,6 +13,9 @@ public class MyName
 	WebElement element;
 	WebDriver web;
 		@Test
+	@Parameters({"link","search","find_id"})
+	public void testCase(String link,String search,String find_id)
+
 		public void testCase()
 		{
 			PropertyConfigurator.configure("log4j.properties");
@@ -20,11 +23,11 @@ public class MyName
 			System.setProperty("webdriver.chrome.driver","chromedriver");
 			web = new ChromeDriver();
 			logs.info("Opening Browser");
-			web.get("http://google.com");
+			web.get(link);
 			logs.info("Getting search tab");
-			element=web.findElement(By.id("lst-ib"));
+			element=web.findElement(By.id(find_id));
 			logs.info("Typing search value");
-			element.sendKeys("Esha Vij");
+			element.sendKeys(search);
 			web.findElement(By.id("_fZl")).click();	
 			logs.info("Closing browser");
 			web.close();
